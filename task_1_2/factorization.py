4c3305f4ec7a8a7800ab19ff3db0321eff924ac8
Dawid <klukowski_dawid@o2.pl>
2022-10-05 19:04:57 +0100

    Code - Refactor

--- task_1_2/factorization.py
+++ task_1_2/factorization.py
@@ -1,19 +1,16 @@
 def prime_factors(number):
     if number <= 1:
         raise ValueError
+
     factors = []
-    i = 0
-    while True:
+    i = 1
+    while number != i:
         i += 1
         while True:
-            if number / i == 1 or number / i == number:
-                pass
-            elif number % i == 0:
+            if number / i != 1 and number % i == 0:
                 number = number // i
                 factors.append(i)
                 continue
-            break
-        if number == i:
             break
     factors.append(number)
     return factors
