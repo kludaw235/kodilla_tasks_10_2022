551ce4845ad7698f0dc02a2a087a872a6bd74970
Dawid <klukowski_dawid@o2.pl>
2022-10-16 14:00:13 +0100

    Quiz - Code - Transfer of the database User class to the separate module\users.py

--- task_2_2/app.py
+++ task_2_2/app.py
@@ -12,19 +12,8 @@ app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 sb = Scoreboard()
 
+from .modules.users import Users
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.Text)
-    is_active = db.Column(db.Boolean)
-    is_admin = db.Column(db.Boolean)
-
-    quizzes = db.relationship('Quiz', backref='Users', lazy='dynamic')
-
-    def __repr__(self):
-        return f'User {self.id}/{self.name}'
 
 
 class Quiz(db.Model):
---
+++ task_2_2/modules/users.py
@@ -0,0 +1,15 @@
+from task_2_2.app import db
+
+class Users(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.Text)
+    is_active = db.Column(db.Boolean)
+    is_admin = db.Column(db.Boolean)
+
+    quizzes = db.relationship('Quiz', backref='Users', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.id}/{self.name}'
