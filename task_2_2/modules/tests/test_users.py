fee4f379714cc06fb3638f2e1735a786cc9bf55f
Dawid <klukowski_dawid@o2.pl>
2022-10-22 18:08:05 +0100

    Quiz - Test - Database Users tests

---
+++ task_2_2/modules/tests/test_users.py
@@ -0,0 +1,14 @@
+from task_2_2.modules.users import Users
+
+def test_Users_import():
+    from task_2_2.modules.users import Users
+    assert callable(Users), '"Users" is not callable'
+
+def test_user():
+    user_id = 1
+    name = "John"
+    email = "fake@hotmail.com"
+    user = Users(id=user_id, name=name, email=email)
+    assert user.id == user_id
+    assert user.name == name
+    assert user.email == email
